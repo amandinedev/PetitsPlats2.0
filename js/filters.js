3a2e8a78bde2cb2f651e280c6d210f7fbb1c8d68
amandinedev <amandinehazard@live.fr>
2025-07-29 11:43:15 +0200

    feature-filter-DOM and event listeners

---
+++ js/filters.js
@@ -0,0 +1,133 @@
+class FilterData {
+  constructor(recipes) {
+    this.recipes = recipes;
+    this.ingredientList = recipes.flatMap((recipe) =>
+      recipe.ingredients.map((ingredient) => ingredient.ingredient)
+    );
+    this.applianceList = recipes.map((recipe) => recipe.appliance);
+
+    this.ustensilList = recipes.flatMap((recipe) => recipe.ustensils);
+  }
+  getIngredientList() {
+    return this.ingredientList;
+  }
+
+  getApplianceList() {
+    return this.applianceList;
+  }
+
+  getUstensilList() {
+    return this.ustensilList;
+  }
+}
+
+// Utility function to remove accents from specific letters
+function transformAccents(str) {
+  return str.toLowerCase().replace(/[éèê]/g, "e");
+}
+
+// Define the base filterTemplate class.
+class FilterTemplate {
+  constructor() {
+    this.filter = "";
+    this.attributeFilter = transformAccents(this.filter);
+  }
+
+  getFilterDOM() {
+    const div = document.createElement("div");
+    div.id = `filter-${this.attributeFilter}`;
+    div.classList.add(
+      "custom-container-filter",
+      "d-flex",
+      "flex-column",
+      "justify-content-start",
+      "me-5",
+      "bg-white",
+      "h-100"
+    );
+    div.innerHTML = `
+     <div id="filter-button-${this.attributeFilter}" class="container button d-flex flex-row justify-content-between w-100 py-4 px-4" role="button" aria-haspopup="listbox" aria-expanded="false" tabindex="0">
+        <label class="fw-medium" for="${this.attributeFilter}">${this.filter}</label>
+        <img src="./assets/icons/icon-arrow-closed.svg" id="filter-closed-${this.attributeFilter}" class="custom-arrow mt-auto mb-auto"> 
+        <img src="./assets/icons/icon-arrow-opened.svg" id="filter-opened-${this.attributeFilter}" class="custom-arrow mt-auto mb-auto d-none"> 
+      </div>
+      <div id="filter-options-${this.attributeFilter}" class="d-flex d-none w-100">
+      <searchbar class="custom-searchbar position-relative d-flex w-100 h-100 mt-2 mb-2">
+        <input id="${this.attributeFilter}" class="custom-input-filter w-100 mt-2 mx-3" type="text"></input>
+        <img src="./assets/icons/icon-loop-light.svg" class="custom-loop-light button position-absolute end-0 bottom-0 p-1 me-4" alt="rechercher" role="button" tabindex="0">
+      </searchbar>
+      </div>
+
+    `;
+    //if input lenght >1 show icon close
+    return div;
+  }
+
+  
+  handleFilterButtonEvent(attributeFilter, filterButton, filterOptions, filterClosedImgElement, filterOpenedImgElement) {
+    
+  
+    const isExpanded =
+    filterButton.getAttribute("aria-expanded") === "true";
+    filterButton.setAttribute("aria-expanded", !isExpanded);
+
+    if (isExpanded) {
+      //open the menu
+      filterClosedImgElement.setAttribute(
+        "aria-label",
+        "ouvrir le menu de filtrage"
+      );
+      filterOptions.classList.remove("d-block");
+      filterOptions.classList.add("d-none");
+      filterClosedImgElement.classList.remove("d-none");
+      filterClosedImgElement.classList.add("d-inline");
+      filterOpenedImgElement.classList.remove("d-inline");
+      filterOpenedImgElement.classList.add("d-none");
+    } else {
+      //close the menu
+    filterOpenedImgElement.setAttribute( 
+        "aria-label",
+        "fermer le menu de filtrage"
+      );
+      // Toggle the display of the filter options
+      filterOptions.classList.remove("d-none");
+      filterOptions.classList.add("d-block");
+      filterOpenedImgElement.classList.remove("d-none");
+      filterOpenedImgElement.classList.add("d-inline");
+      filterClosedImgElement.classList.add("d-none");
+    }
+  }
+}
+
+
+class FilterMenuIngredients extends FilterTemplate {
+  constructor() {
+    super();
+    this.filter = "Ingrédients";
+    this.attributeFilter = transformAccents(this.filter);
+  }
+}
+
+class FilterMenuAppliances extends FilterTemplate {
+  constructor() {
+    super();
+    this.filter = "Appareils";
+    this.attributeFilter = transformAccents(this.filter);
+  }
+}
+
+class FilterMenuUstensils extends FilterTemplate {
+  constructor() {
+    super();
+    this.filter = "Ustensiles";
+    this.attributeFilter = transformAccents(this.filter);
+  }
+}
+
+const filterData = new FilterData(recipes);
+
+console.log(filterData.getIngredientList());
+console.log(filterData.getApplianceList());
+console.log(filterData.getUstensilList());
+
+
--- js/index.js
+++ js/index.js
@@ -1,11 +1,56 @@
 async function displayData() {
-    const headerDisplay = headerTemplate();
+  //header
+  const headerDisplay = headerTemplate();
+
+  //filters
+  const main = document.getElementById("main");
+  main.classList.add("px-5","py-3")
+  const sectionFilters = document.createElement("section");
+  main.appendChild(sectionFilters);
+  sectionFilters.classList.add(
+    "section-filters",
+    "container",
+    "d-flex",
+    "flex-row", 
+    "justify-content-start"
+  );
+  const filterMenuIngredients = new FilterMenuIngredients();
+  const filterMenuAppliances = new FilterMenuAppliances();
+  const filterMenuUstensils = new FilterMenuUstensils();
+ 
+  const filters = [
+    { filter: filterMenuIngredients, attributeFilter: filterMenuIngredients.attributeFilter },
+    { filter: filterMenuAppliances, attributeFilter: filterMenuAppliances.attributeFilter },
+    { filter: filterMenuUstensils, attributeFilter: filterMenuUstensils.attributeFilter },
+    
+  ];
+
+  filters.forEach(({ filter }) => {
+    sectionFilters.appendChild(filter.getFilterDOM());
+  });
+
+  // Event listeners
+  const filterTemplateInstance = new FilterTemplate();
+
+  filters.forEach(({ attributeFilter }) => {
+    const filterButton = document.getElementById(`filter-button-${attributeFilter}`);
+    const filterOptions = document.getElementById(`filter-options-${attributeFilter}`);
+    const filterClosedImgElement = document.getElementById(`filter-closed-${attributeFilter}`);
+    const filterOpenedImgElement = document.getElementById(`filter-opened-${attributeFilter}`);
+
+    filterButton.addEventListener("click", () => filterTemplateInstance.handleFilterButtonEvent(attributeFilter, filterButton, filterOptions, filterClosedImgElement, filterOpenedImgElement));
+    filterButton.addEventListener("keydown", function (event) {
+      if (event.key === "Enter" || event.key === "Space") {
+        filterTemplateInstance.handleFilterButtonEvent(attributeFilter, filterButton, filterOptions, filterClosedImgElement, filterOpenedImgElement);
+      }
+    });
+  });
 }
 
 async function init() {
-    try{
-        await displayData();
-    } catch (error) {
+  try {
+    await displayData();
+  } catch (error) {
     console.error("An error occurred:", error);
   }
 }
